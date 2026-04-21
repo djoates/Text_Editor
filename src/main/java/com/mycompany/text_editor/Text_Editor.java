a5c9f8c15cfe43891f36b9dd2bbfa694f9412552
doates3 <doates3@student.gsu.edu>
2026-04-21 14:03:08 -0400

    Project Initialization

---
+++ src/main/java/com/mycompany/text_editor/Text_Editor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.text_editor;
+
+/**
+ *
+ * @author djzon
+ */
+public class Text_Editor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
