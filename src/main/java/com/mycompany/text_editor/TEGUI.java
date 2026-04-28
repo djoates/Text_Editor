d6b84ecf4605202787497c96db13b195a4ceeb8e
Ajalmeida-hue123 <25almeidaa@gmail.com>
2026-04-28 16:46:35 -0400

    Adding undo and redo

--- src/main/java/com/mycompany/text_editor/TEGUI.java
+++ src/main/java/com/mycompany/text_editor/TEGUI.java
@@ -4,6 +4,9 @@
  */
 package com.mycompany.text_editor;
 
+import java.awt.Font;
+import javax.swing.undo.UndoManager;
+
 /**
  *
  * @author Alex
@@ -17,7 +20,11 @@ public class TEGUI extends javax.swing.JFrame {
      */
     public TEGUI() {
         initComponents();
+        jTextfield.getDocument().addUndoableEditListener(undoManager);
+
     }
+    private UndoManager undoManager = new UndoManager();
+    public boolean bold = false;
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -35,6 +42,7 @@ public class TEGUI extends javax.swing.JFrame {
         jToolBar1 = new javax.swing.JToolBar();
         jundo = new javax.swing.JButton();
         jredo = new javax.swing.JButton();
+        jBold_btn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,10 +61,17 @@ public class TEGUI extends javax.swing.JFrame {
         jToolBar1.setRollover(true);
 
         jundo.setText("Undo");
+        jundo.addActionListener(this::jundoActionPerformed);
         jToolBar1.add(jundo);
 
         jredo.setText("Redo");
+        jredo.addActionListener(this::jredoActionPerformed);
         jToolBar1.add(jredo);
+
+        jBold_btn.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jBold_btn.setText("Bold");
+        jBold_btn.addActionListener(this::jBold_btnActionPerformed);
+        jToolBar1.add(jBold_btn);
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -68,12 +83,10 @@ public class TEGUI extends javax.swing.JFrame {
                 .addGap(91, 91, 91))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 668, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 668, Short.MAX_VALUE))
                 .addGap(31, 31, 31))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -83,7 +96,7 @@ public class TEGUI extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jSAVEBT, javax.swing.GroupLayout.PREFERRED_SIZE, 21, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(7, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -111,6 +124,36 @@ public class TEGUI extends javax.swing.JFrame {
 
     }//GEN-LAST:event_jSAVEBTMousePressed
 
+    private void jundoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jundoActionPerformed
+        // TODO add your handling code here:
+        if (undoManager.canUndo()) {
+            undoManager.undo();
+        }
+
+    }//GEN-LAST:event_jundoActionPerformed
+
+    private void jredoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jredoActionPerformed
+        // TODO add your handling code here:
+        if (undoManager.canRedo()) {
+            undoManager.redo();
+        }
+    }//GEN-LAST:event_jredoActionPerformed
+
+    private void jBold_btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBold_btnActionPerformed
+        // TODO add your handling code here:
+        // get the curent font
+
+        if (!bold) {
+            Font currentFont = jTextfield.getFont();
+            // make it bold based on what is currently in the text area
+            Font boldFont = new Font(currentFont.getName(), Font.BOLD, currentFont.getSize());
+            // next apply it
+            jTextfield.setFont(boldFont);
+        }
+
+
+    }//GEN-LAST:event_jBold_btnActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -137,6 +180,7 @@ public class TEGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jBold_btn;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jSAVEBT;
     private javax.swing.JScrollPane jScrollPane1;
